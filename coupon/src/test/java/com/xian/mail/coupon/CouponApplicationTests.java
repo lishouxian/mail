0157de8ca8c31a262aaee644783331cb7b388a75
lishouxian <li.shouxian@outlook.com>
2020-09-15 15:33:50 +0800

    项目初始结构创建

---
+++ coupon/src/test/java/com/xian/mail/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package xom.xian.mail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
