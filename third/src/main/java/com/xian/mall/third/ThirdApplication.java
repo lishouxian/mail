56b2187a640c497335927ed30042e56ca9eb8f32
lishouxian <li.shouxian@outlook.com>
2020-09-24 22:06:28 +0800

    new update

---
+++ third/src/main/java/com/xian/mall/third/ThirdApplication.java
@@ -0,0 +1,14 @@
+package com.xian.mall.third;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ThirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdApplication.class, args);
+	}
+
+}
